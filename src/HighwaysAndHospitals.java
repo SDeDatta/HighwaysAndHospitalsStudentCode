f91378da14d5a681598a0319caf691816553c069
suryadedatta <surya.dedatta@menloschool.org>
2025-09-15 16:32:38 -0700

    Wrote 2D list and wrote DFS to reveal cluster count

--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 /**
  * Highways & Hospitals
  * A puzzle created by Zach Blick
@@ -17,11 +19,42 @@ public class HighwaysAndHospitals {
     public static long cost(int n, int hospitalCost, int highwayCost, int cities[][])
     {
         // Base Case
-        if(highwayCost > hospitalCost)
+        List<List<Integer>> list = new ArrayList<>();
+        for(int [] city : cities)
         {
-            return n*hospitalCost;
+            int num = city[0];
+            int num2 = city[1];
+            list.get(num).add(num2);
+            list.get(num2).add(num);
+        }
+        Stack<Integer> myStack = new Stack<Integer>();
+        boolean[] visited = new boolean[n+1];
+        int clusterCount = 0;
+        int current = 0;
+        for(int i = 1; i <= n; i++)
+        {
+            if(!visited[i])
+            {
+                myStack.push(0);
+                visited[1] = true;
+                while(!myStack.empty())
+                {
+                    current = myStack.pop();
+                    for(int neighbor : list.get(current))
+                    {
+                        if(!visited[neighbor])
+                        {
+                            visited[neighbor] = true;
+                            myStack.push(neighbor);
+                        }
+                    }
+                    clusterCount++;
+                }
+            }
         }
 
+        // Adjacency matrix or use DFS to
+        // Figure out how many clusters (number of cities-1*highway cost)
         // 1 hospital more worth than 2 highways if the highways simply connect one more city
 
         // Try making it so you build all possible highways and see the minimum number of hospitals you need
