840233f16b8d80afe13745b22b921d715f6ddba2
suryadedatta <surya.dedatta@menloschool.org>
2025-09-22 09:54:41 -0700

    Started writing comments and completed correctness

--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -10,28 +10,27 @@ import java.util.*;
  *
  */
 
-public class HighwaysAndHospitals {
-
-    /**
-     * TODO: Complete this function, cost(), to return the minimum cost to provide
-     *  hospital access for all citizens in Menlo County.
-     */
+public class HighwaysAndHospitals
+{
     public static long cost(int n, int hospitalCost, int highwayCost, int cities[][])
     {
         // Base Case
         if(highwayCost > hospitalCost)
         {
-            return n*hospitalCost;
+            return (long) n*hospitalCost;
         }
+        // Array to represent all the cities and what weights they hold/cities that are roots of them
         long[] arr = new long[n+1];
         for(int i = 1; i < arr.length;i++)
         {
+            // Sets every city to have a weight of -1;
             arr[i] = -1;
         }
+        // Runs the union function for every possible city connection
         for(int edge[] : cities) {
             union(edge[0], edge[1], arr);
         }
-        int discComponents = 0;
+        // Calculate the total cost
         long total = 0;
         for(int i = 1; i < arr.length;i++)
         {
